b594f03ffe90a0dcdbb1f4ae44c05ec55809e8e9
Maryie <mashenka.zinkevich0245@gmail.com>
2024-06-04 21:03:24 +0300

    The code is updated

--- src/layout/header/sections/main/Main.tsx
+++ src/layout/header/sections/main/Main.tsx
@@ -1,15 +1,28 @@
 import React from "react";
 import { Button } from "../../../../assets/Button";
+import styled from "styled-components";
+import myPhoto from "../../../../assets/images/me-photo-my.jpg";
 
 export const Main = () => {
     return (
-        <div>
+        <StyledMain>
             <div>
                 <h1>Maryia is a Web Developer.</h1>
-                <h2>He crafts responsive websites where technologies meet creativity</h2>
+                <h2>She crafts responsive websites where technologies meet creativity</h2>
                 <Button/>
-
             </div>
-        </div>
+            <Photo src={myPhoto} alt=""/>            
+        </StyledMain>
     )
 }
+
+const StyledMain = styled.div`
+min-heignt: 100vh;
+background-color: grey;
+`
+
+const Photo = styled.img`
+width: 380px;
+height: 310px;
+object-fit: cover;
+`
