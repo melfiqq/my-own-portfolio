861207b8e383d8fc4f85571e79d8ee92f122783c
Maryie <mashenka.zinkevich0245@gmail.com>
2024-06-04 20:47:13 +0300

    The code is updated

---
+++ src/components/menu/Menu.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "styled-components";
+
+export const Menu = () => {
+    return (
+        <StyledMenu>
+            <ul>
+                <li>
+                    <a href="">home</a>
+                </li>
+                <li>
+                    <a href="">works</a>
+                </li>
+                <li>
+                    <a href="">about-me</a>
+                </li>
+                <li>
+                    <a href="">contacts</a>
+                </li>
+                <li>
+                    <a href="">EN</a>
+                </li>
+            </ul>
+
+        </StyledMenu>
+    )
+}
+
+const StyledMenu = styled.nav`
+ul{
+display: flex;
+gap: 30px;    
+}`
---
+++ src/layout/header/sections/main/Main.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Button } from "../../../../assets/Button";
+
+export const Main = () => {
+    return (
+        <div>
+            <div>
+                <h1>Maryia is a Web Developer.</h1>
+                <h2>He crafts responsive websites where technologies meet creativity</h2>
+                <Button/>
+
+            </div>
+        </div>
+    )
+}
